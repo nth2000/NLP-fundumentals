688bbd112907afcad8b0428cd8ddbddb7494b8b7
nth2000 <nth1220@126.com>
2022-07-23 11:38:42 +0800

    1

--- cs224n-proj/p3/a3/run.py
+++ cs224n-proj/p3/a3/run.py
@@ -51,10 +51,10 @@ def train(parser, train_data, dev_data, output_path, batch_size=1024, n_epochs=1
     ###       necessary parameters to tune.
     ### Please see the following docs for support:
     ###     Adam Optimizer: https://pytorch.org/docs/stable/optim.html
-    ###     Cross Entropy Loss: https://pytorch.org/docs/stable/nn.html#crossentropyloss
+    ###     Cross Entropy Loss: httsps://pytorch.org/docs/stable/nn.html#crossentropyloss
 
-
-
+    loss_func = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(parser.model.parameters())
     ### END YOUR CODE
 
     for epoch in range(n_epochs):
@@ -105,10 +105,10 @@ def train_for_epoch(parser, train_data, dev_data, optimizer, loss_func, batch_si
             ###      4) Take step with the optimizer
             ### Please see the following docs for support:
             ###     Optimizer Step: https://pytorch.org/docs/stable/optim.html#optimizer-step
-
-
-
-
+            logits = parser.model(train_x)
+            loss = loss_func(logits,train_y)
+            loss.backward()
+            optimizer.step()
             ### END YOUR CODE
             prog.update(1)
             loss_meter.update(loss.item())
